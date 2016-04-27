36f867a76c589bb0b23a96f5f24019c3b31c3046
kazuya030 <kazuya030@gmail.com>
2016-04-27 22:58:20 +0900

    Remove sample_tools
    replaces gooleapiclient.sample_tools with standard googleapiclent codes

--- gsc.py
+++ gsc.py
@@ -1,96 +1,79 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This code is changed from
+# https://github.com/google/google-api-python-client/blob/0eaa280ba12a28fac16ff72e9ffaafc4016ab901/samples/searchconsole/search_analytics_api_sample.py
 
-"""Example for using the Google Search Analytics API (part of Search Console API).
-
-A basic python command-line example that uses the searchAnalytics.query method
-of the Google Search Console API. This example demonstrates how to query Google
-search results data for your property. Learn more at
-https://developers.google.com/webmaster-tools/
-
-To use:
-1) Install the Google Python client library, as shown at https://developers.google.com/webmaster-tools/v3/libraries.
-2) Sign up for a new project in the Google APIs console at https://code.google.com/apis/console.
-3) Register the project to use OAuth2.0 for installed applications.
-4) Copy your client ID, client secret, and redirect URL into the client_secrets.json file included in this package.
-5) Run the app in the command-line as shown below.
-
-Sample usage:
-
-  $ python search_analytics_api_sample.py 'https://www.example.com/' '2015-05-01' '2015-05-30'
-
-"""
-
-import argparse
 import sys
-from googleapiclient import sample_tools
+import os
+import httplib2
+import datetime
+from oauth2client import client
+from oauth2client import tools
+from oauth2client import file
+from googleapiclient import discovery
 
-# Declare command-line flags.
-argparser = argparse.ArgumentParser(add_help=False)
-argparser.add_argument('property_uri', type=str,
-                       help=('Site or app URI to query data for (including '
-                             'trailing slash).'))
-argparser.add_argument('start_date', type=str,
-                       help=('Start date of the requested date range in '
-                             'YYYY-MM-DD format.'))
-argparser.add_argument('end_date', type=str,
-                       help=('End date of the requested date range in '
-                             'YYYY-MM-DD format.'))
+def main():
+  name = 'webmasters'
 
+  client_secrets = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
 
-def main(argv):
-  service, flags = sample_tools.init(
-      argv, 'webmasters', 'v3', __doc__, __file__, parents=[argparser],
-      scope='https://www.googleapis.com/auth/webmasters.readonly')
+  flow = client.flow_from_clientsecrets(client_secrets,
+      scope='https://www.googleapis.com/auth/webmasters.readonly',
+      message=tools.message_if_missing(client_secrets))
+
+  storage = file.Storage(name + '.dat')
+  credentials = storage.get()
+  if credentials is None or credentials.invalid:
+    credentials = tools.run_flow(flow, storage)
+
+  # Create an httplib2.Http object and authorize it with our credentials
+  http = credentials.authorize(http = httplib2.Http())
+  service = discovery.build(name, 'v3', http=http)
+  site_list = service.sites().list().execute()
+  verified_sites_urls = [s['siteUrl'] for s in site_list['siteEntry'] if s['permissionLevel'] != 'siteUnverifiedUser']
+
+  print(verified_sites_urls)
+
+  start_date = (datetime.date.today()-datetime.timedelta(weeks=4)).strftime('%Y-%m-%d')
+  end_date =  datetime.date.today().strftime('%Y-%m-%d')
+  property_uri = verified_sites_urls[0]
+
 
   # First run a query to learn which dates we have data for. You should always
   # check which days in a date range have data before running your main query.
   # This query shows data for the entire range, grouped and sorted by day,
   # descending; any days without data will be missing from the results.
   request = {
-      'startDate': flags.start_date,
-      'endDate': flags.end_date,
+      'startDate': start_date,
+      'endDate': end_date,
       'dimensions': ['date']
   }
-  response = execute_request(service, flags.property_uri, request)
+  response = execute_request(service, property_uri, request)
   print_table(response, 'Available dates')
 
   # Get totals for the date range.
   request = {
-      'startDate': flags.start_date,
-      'endDate': flags.end_date
+      'startDate': start_date,
+      'endDate': end_date
   }
-  response = execute_request(service, flags.property_uri, request)
+  response = execute_request(service, property_uri, request)
   print_table(response, 'Totals')
 
   # Get top 10 queries for the date range, sorted by click count, descending.
   request = {
-      'startDate': flags.start_date,
-      'endDate': flags.end_date,
+      'startDate': start_date,
+      'endDate': end_date,
       'dimensions': ['query'],
       'rowLimit': 10
   }
-  response = execute_request(service, flags.property_uri, request)
+  response = execute_request(service, property_uri, request)
   print_table(response, 'Top Queries')
 
   # Get top 11-20 mobile queries for the date range, sorted by click count, descending.
   request = {
-      'startDate': flags.start_date,
-      'endDate': flags.end_date,
+      'startDate': start_date,
+      'endDate': end_date,
       'dimensions': ['query'],
       'dimensionFilterGroups': [{
           'filters': [{
@@ -101,23 +84,23 @@ def main(argv):
       'rowLimit': 10,
       'startRow': 10
   }
-  response = execute_request(service, flags.property_uri, request)
+  response = execute_request(service, property_uri, request)
   print_table(response, 'Top 11-20 Mobile Queries')
 
   # Get top 10 pages for the date range, sorted by click count, descending.
   request = {
-      'startDate': flags.start_date,
-      'endDate': flags.end_date,
+      'startDate': start_date,
+      'endDate': end_date,
       'dimensions': ['page'],
       'rowLimit': 10
   }
-  response = execute_request(service, flags.property_uri, request)
+  response = execute_request(service, property_uri, request)
   print_table(response, 'Top Pages')
 
   # Get the top 10 queries in India, sorted by click count, descending.
   request = {
-      'startDate': flags.start_date,
-      'endDate': flags.end_date,
+      'startDate': start_date,
+      'endDate': end_date,
       'dimensions': ['query'],
       'dimensionFilterGroups': [{
           'filters': [{
@@ -127,17 +110,17 @@ def main(argv):
       }],
       'rowLimit': 10
   }
-  response = execute_request(service, flags.property_uri, request)
+  response = execute_request(service, property_uri, request)
   print_table(response, 'Top queries in India')
 
   # Group by both country and device.
   request = {
-      'startDate': flags.start_date,
-      'endDate': flags.end_date,
+      'startDate': start_date,
+      'endDate': end_date,
       'dimensions': ['country', 'device'],
       'rowLimit': 10
   }
-  response = execute_request(service, flags.property_uri, request)
+  response = execute_request(service, property_uri, request)
   print_table(response, 'Group by country and device')
 
 
@@ -178,12 +161,12 @@ def print_table(response, title):
     keys = ''
     # Keys are returned only if one or more dimensions are requested.
     if 'keys' in row:
-      keys = u','.join(row['keys']).encode('utf-8')
+      keys = ','.join(row['keys'])
     #print(row_format.format(
     #    keys, row['clicks'], row['impressions'], row['ctr'], row['position']))
-    # print(row['keys'])
-    print(row_format.format(1, row['clicks'], row['impressions'], row['ctr'], row['position']))
+    #  print(row['keys'])
+    print(row_format.format(keys, row['clicks'], row['impressions'], row['ctr'], row['position']))
 
 
 if __name__ == '__main__':
-  main(sys.argv)
+  main()
